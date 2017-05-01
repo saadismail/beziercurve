d9f18410743ed61a4625ae67a4dd9590ace3ee4a
Saad Ismail <saad3112@gmail.com>
2017-05-01 17:43:42 +0500

    At last, a perfect commit :)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "point.h"
 #include <iostream>
+#include "point.h"
 
 Point p[4] = {Point(4,1), Point(28,48), Point(50,42), Point(40,5)};
 
@@ -24,29 +24,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->customPlot->xAxis->setRange(-10, 100);
     ui->customPlot->yAxis->setRange(-10, 100);
 
-    ui->p0x->setValue(p[0].getX());
-    ui->p1x->setValue(p[1].getX());
-    ui->p2x->setValue(p[2].getX());
-    ui->p3x->setValue(p[3].getX());
-    ui->p0y->setValue(p[0].getY());
-    ui->p1y->setValue(p[1].getY());
-    ui->p2y->setValue(p[2].getY());
-    ui->p3y->setValue(p[3].getY());
+    ui->p0x->setValue(4);
+    ui->p1x->setValue(28);
+    ui->p2x->setValue(50);
+    ui->p3x->setValue(40);
+    ui->p0y->setValue(1);
+    ui->p1y->setValue(48);
+    ui->p2y->setValue(42);
+    ui->p3y->setValue(5);
 
-    double diff;
-    for (double t=0; t<=1.01; t=t+0.01) {
-        diff = (double)1 - t;
-        x << (p[0].getX() * diff*diff*diff) + (3 * p[1].getX() * t * diff*diff) + (3 * p[2].getX() * t*t * diff) + (p[3].getX() * t*t*t);
-        y << (p[0].getY() * diff*diff*diff) + (3 * p[1].getY() * t * diff*diff) + (3 * p[2].getY() * t*t * diff) + (p[3].getY() * t*t*t);
-    }
-    for (int i=0; i<2; i++) {
-        l1x << p[i].getX();
-        l1y << p[i].getY();
-        l2x << p[i+1].getX();
-        l2y << p[i+1].getY();
-        l3x <<  p[i+2].getX();
-        l3y << p[i+2].getY();
-    }
+
+    // Connect buttons
+    connect(ui->q1, SIGNAL (released()), this, SLOT (handleq1()));
+    connect(ui->q3, SIGNAL (released()), this, SLOT (handleq3()));
+    connect(ui->q4, SIGNAL (released()), this, SLOT (handleq4()));
 
     ui->p0x->setKeyboardTracking(false);
     connect(ui->p0x, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),[=](double d){ p[0].setX(d); this->reGraph(); });
@@ -63,12 +54,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->customPlot->yAxis->setLabel("y");
     // Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
     ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
-
-    this->mainCurve->setData(x, y);
-    ui->customPlot->graph(0)->setData(l1x, l1y);
-    ui->customPlot->graph(1)->setData(l2x, l2y);
-    ui->customPlot->graph(2)->setData(l3x, l3y);
-    ui->customPlot->replot();
 }
 
 void MainWindow::reGraph() {
@@ -84,24 +69,63 @@ void MainWindow::reGraph() {
     double diff;
     for (double t=0; t<=1.01; t=t+0.01) {
         diff = (double)1 - t;
-        x << (p[0].getX() * diff*diff*diff) + (3 * p[1].getX() * t * diff*diff) + (3 * p[2].getX() * t*t * diff) + (p[3].getX() * t*t*t);
-        y << (p[0].getY() * diff*diff*diff) + (3 * p[1].getY() * t * diff*diff) + (3 * p[2].getY() * t*t * diff) + (p[3].getY() * t*t*t);
+        x << (ui->p0x->value() * diff*diff*diff) + (3 * ui->p1x->value() * t * diff*diff) + (3 * ui->p2x->value() * t*t * diff) + (ui->p3x->value() * t*t*t);
+        y << (ui->p0y->value() * diff*diff*diff) + (3 * ui->p1y->value() * t * diff*diff) + (3 * ui->p2y->value() * t*t * diff) + (ui->p3y->value() * t*t*t);
     }
 
-    for (int i=0; i<2; i++) {
-        l1x << p[i].getX();
-        l1y << p[i].getY();
-        l2x << p[i+1].getX();
-        l2y << p[i+1].getY();
-        l3x <<  p[i+2].getX();
-        l3y << p[i+2].getY();
-    }
+    l1x << ui->p0x->value();
+    l1x << ui->p1x->value();
+    l1y << ui->p0y->value();
+    l1y << ui->p1y->value();
+    l2x << ui->p1x->value();
+    l2x << ui->p2x->value();
+    l2y << ui->p1y->value();
+    l2y << ui->p2y->value();
+    l3x << ui->p2x->value();
+    l3x << ui->p3x->value();
+    l3y << ui->p2y->value();
+    l3y << ui->p3y->value();
 
     this->mainCurve->setData(x, y);
     ui->customPlot->graph(0)->setData(l1x, l1y);
     ui->customPlot->graph(1)->setData(l2x, l2y);
     ui->customPlot->graph(2)->setData(l3x, l3y);
     ui->customPlot->replot();
+}
+
+void MainWindow::handleq1() {
+    ui->p0x->setValue(4);
+    ui->p1x->setValue(28);
+    ui->p2x->setValue(50);
+    ui->p3x->setValue(40);
+    ui->p0y->setValue(1);
+    ui->p1y->setValue(48);
+    ui->p2y->setValue(42);
+    ui->p3y->setValue(5);
+    this->reGraph();
+}
+
+void MainWindow::handleq3() {
+    ui->p0x->setValue(4);
+    ui->p1x->setValue(80);
+    ui->p2x->setValue(50);
+    ui->p3x->setValue(40);
+    ui->p0y->setValue(1);
+    ui->p1y->setValue(10);
+    ui->p2y->setValue(42);
+    ui->p3y->setValue(5);
+    this->reGraph();
+}
+void MainWindow::handleq4() {
+    ui->p0x->setValue(80);
+    ui->p1x->setValue(20);
+    ui->p2x->setValue(20);
+    ui->p3x->setValue(80);
+    ui->p0y->setValue(80);
+    ui->p1y->setValue(90);
+    ui->p2y->setValue(10);
+    ui->p3y->setValue(20);
+    this->reGraph();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -16,12 +16,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 private slots:
-    void reGraph();
+    void handleq1();
+    void handleq3();
+    void handleq4();
 
 private:
     Ui::MainWindow *ui;
     QCPCurve *mainCurve;
     QVector <double> x, y, l1x, l1y, l2x, l2y, l3x, l3y;
+    void reGraph();
 };
 
 #endif // MAINWINDOW_H
